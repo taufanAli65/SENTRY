a8e40facb27f0e997b7b5e8873e12abc76ab7c1a
taufanAli65 <taufanali65@gmail.com>
2025-06-04 08:44:22 +0700

    feat: add user model with roles and validation schema
    - Added a Mongoose user model () with a schema that includes email, name, role, and photoUrl fields.
    - Defined a  enum to restrict user roles to owner, admin, or employee.
    - Implemented validation for email format, uniqueness, and required fields.
    - Set up TypeScript interface () for type safety and consistency.
    - Ensured the role field defaults to 'employee' and only accepts valid enum values.

---
+++ api/models/users.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+
+export enum UserRoles {
+    Owner = 'owner',
+    Admin = 'admin',
+    Employee = 'employee',
+}
+
+export interface IUser extends Document {
+    email: string,
+    name: string,
+    role: UserRoles,
+    photoUrl: string,
+}
+
+const userSchema = new Schema({
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Email is invalid'],
+    }, 
+    name: {type: String, required: true},
+    role : {
+        type: String,
+        enum: Object.values(UserRoles),
+        required: true,
+        default: UserRoles.Employee,
+    },
+    photoUrl: {type: String, required: true},
+});
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
