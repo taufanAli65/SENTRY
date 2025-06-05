ad8bd0a0a92306ed2305c073fa029939004ef5d5
taufanAli65 <taufanali65@gmail.com>
2025-06-05 12:29:49 +0700

    fix: Update error message for invalid ObjectId validation on item

--- api/validator/item_validator.ts
+++ api/validator/item_validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import mongoose from 'mongoose';
 
 const objectIdSchema = z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
-    message: "Invalid ObjectId",
+    message: "Invalid data type",
 });
 
 export const addItemSchema = z.object({
