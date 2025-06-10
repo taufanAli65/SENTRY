a40e9e811a247d15762ceba6efbb431a048f1b29
taufanAli65 <taufanali65@gmail.com>
2025-06-10 10:23:09 +0700

    fix: Remove unused import from Scans model

--- api/models/scans.ts
+++ api/models/scans.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document, Types, models } from 'mongoose';
+import { Schema, model, Document, Types } from 'mongoose';
 
 export interface iScan extends Document {
     id_user: Types.ObjectId,
